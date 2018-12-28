b6fa190025a0b1b8fe14311650915b6dfb18cd26
raulmejia <r8a8m8p8@gmail.com>
2018-12-28 15:53:22 -0600

    changing variables to encoding and input files instead of chain of characters

--- replaceFx_by_acentos.sh
+++ replaceFx_by_acentos.sh
@@ -2,4 +2,8 @@ input_file=../Data/INEGI_Mortalidad_2010/Mortalidad_2010_completo.csv
 
 output_file=../Data/INEGI_Mortalidad_2010/Mortalidad_2010_completo.csv.UTF8
 
+initial_encoding=$(file -i $input_file | awk -F'[=]' '{print $2}')
+
+iconv -f $initial_encoding -t UTF-8 < $input_file > $output_file
+
 iconv -f iso-8859-1 -t UTF-8 < $input_file > $output_file
