e8b2f248b3bd4f2c6fbee98a7fd03395dae367ce
raulmejia <r8a8m8p8@gmail.com>
2018-12-31 21:26:45 -0600

    erase two duplicated rows in 2010 data

--- Exploring_and_Discover_Patterns.R
+++ Exploring_and_Discover_Patterns.R
@@ -2,34 +2,36 @@
 source("libraries/plot_raw_Matrix_png.R")
 
 #-- Data changed by the user 
-mymat_path <- c("../Data/INEGI_Mortalidad_2010/Mortalidad_2010_completo.csv.UTF8")
-# mymat_path <- c("../Data/INEGI_Mortalidad_2010/Mortalidad_2010_completo.UTF8.tsv")
+mymat_path <- c("../Data/INEGI_Mortalidad_2010/Mor2010_UTF8_imputed_zeros.csv")
+#mymat_path <- c("../Data/INEGI_Mortalidad_2010/Mortalidad_2010_completo.UTF8.tsv")
 title_heatmap<- c("Death cause in mexican municipalities")
 results_path <- c("/media/rmejia/ADATA-HD710/boba-bk-postsismo/rmejia/Documents/Otros_Proyectos_academicos/Municipios_CIE10_vs_MunMex/Results/")
 file_name <- paste0( results_path,"Heatmap_and_Dendro_absolute_deaths_vs_Municipalities_including_States.png"  )
 # don't forget change the column_colors
 
 #-- Loading data
-mymat <- read.csv(file=mymat_path )
+mymat <- read.csv(file=mymat_path)
+mymat <- as.matrix(mymat)
 #mymat <- read.table(file=mymat_path, quote="", header=TRUE, sep="\t", stringsAsFactors=FALSE )
+
 dim(mymat)
 class(mymat)
-
-is.na[mymat]
-
-mymat<- as.data.frame(mymat)
-class[mymat]
-str(mymat)
-dim[mymat]
 mymat[1:16,1:6]
 column_colors <- rep("green",dim(mymat)[2])
 
 #-- Cleaning the data
-mymat == NA 
 
 
 #-- Invoking the function
 
+mymat <- as.numeric(mymat)
+is.na(mymat)<-0
+str(mymat)
+head(mymat)
+mymat[is.na(mymat)]
+
 plot_raw_Matrix_png(mymat, title_heatmap,file_name , column_colors)
 
+heatmap(mymat)
+str(mymat)
 
--- replaceFx_by_acentos.sh
+++ replaceFx_by_acentos.sh
@@ -1,12 +1,20 @@
 input_file=../Data/INEGI_Mortalidad_2010/Mortalidad_2010_completo.csv
 output_file=../Data/INEGI_Mortalidad_2010/Mortalidad_2010_completo.csv.UTF8
 initial_encoding=$(file -i $input_file | awk -F'[=]' '{print $2}')
-iconv -f $initial_encoding -t UTF-8 < $input_file > $output_file
+iconv -f $initial_encoding -t UTF-8 < $input_file > $output_file	
+sed -i '$ d' $output_file
+sed -i '3423d' $output_file
+
+
+output_file_zeros="../Data/INEGI_Mortalidad_2010/Mor2010_UTF8_imputed_zeros.csv"
+cp $output_file $output_file_zeros
+sed -i "s/,,/,0,/g" $output_file_zeros
+sed -i "s/,,/,0,/g" $output_file_zeros
 
 # iconv -f iso-8859-1 -t UTF-8 < $input_file > $output_file
 
 out_tsv="../Data/INEGI_Mortalidad_2010/Mortalidad_2010_completo.UTF8.tsv"
-sed 's/, / /g'  < $output_file > $out_tsv
+sed 's/, / /g'  < $output_file_zeros > $out_tsv
 sed -i "s/,/\t/g" $out_tsv
 sed -i 's/"//g' $out_tsv
 
